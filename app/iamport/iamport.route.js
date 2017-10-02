c5e4f0264f893af09985d3da059f12e51563be34
Jun Park <jun.parkc@gmail.com>
2017-10-02 14:35:12 -0400

    Update comments

--- app/iamport/iamport.route.js
+++ app/iamport/iamport.route.js
@@ -11,11 +11,13 @@ router.route('/token')
         iamportService.getToken(req, res);
     });
 
-
-/* 
-Provide the `customer_uid` to the path variable `:customer_uid` ( customer_uid = {business_id} + '_' + {card_last_4_digits} ).
-Once we call POST /customer/:customer_uid, we can use the `customer_uid` to request payment using the corresponding payment method.
-*/
+/**
+ * This route is used to manage payment methods. Each POST (with unique `customer_uid`) will create ONE payment method. 
+ * Every `customer_uid` should be mapped to corresponding business (via `business_id`) on our side, as I'mport does not provide such feature.
+ * 
+ * - Provide the `customer_uid` to the path variable `:customer_uid` ( customer_uid = {business_id} + '_' + {card_last_4_digits} ).
+ * - Once we call POST /customer/:customer_uid, we can use the `customer_uid` to request payment using the corresponding payment method.
+ */
 router.route('/customer/:customer_uid')
     .get(function (req, res) {
         // Uses 'customer_uid' to retrieve the payment method
@@ -30,18 +32,18 @@ router.route('/customer/:customer_uid')
         iamportService.deleteCustomer(req, res);
     });
 
-/* 
-Use the business_id (formerly restaurant_id) for the `merchant_uid`.
-This way, we can retrieve all the transaction history for a specific `merchant_uid` (which is mapped to a specific business).
-*/
+/** 
+ * Use the business_id (formerly restaurant_id) for the `merchant_uid`.
+ * This way, we can retrieve all the transaction history for a specific `merchant_uid` (which is mapped to a specific business).
+ */
 router.post('/pay/:customer_uid', function (req, res) {
     // Processes a one-time payment with the provided `customer_uid`
     iamportService.pay(req, res);
 });
 
 /*
-Use the business_id (formerly restaurant_id) for the `merchant_uid`.
-This will retrieve all the transaction history for a specific `merchant_uid` (which is mapped to a specific business).
+ * Use the business_id (formerly restaurant_id) for the `merchant_uid`.
+ * This will retrieve all the transaction history for a specific `merchant_uid` (which is mapped to a specific business).
 */
 router.get('/history/:merchant_uid', function (req, res) {
     // Retrieve all transaction hisotry for the provided `merchant_uid`
