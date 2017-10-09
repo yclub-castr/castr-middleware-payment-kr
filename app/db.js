0314783e12e0bc5c98e249c92630addb11f7181b
Jun Park <jun.parkc@gmail.com>
2017-10-09 11:19:26 +0900

    Clean code

--- app/db.js
+++ app/db.js
@@ -3,13 +3,14 @@ const MongoClient = require('mongodb');
 
 module.exports = {
     connect: function (callback) {
-        MongoClient.connect(
-            'mongodb://' + process.env.DB_USER + ':' + process.env.DB_PW + '@ds123400.mlab.com:23400/' + process.env.DB_HOST,
-            (err, db) => {
+        MongoClient.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PW + '@ds123400.mlab.com:23400/' + process.env.DB_HOST)
+            .then(function (db) {
                 _db = db;
+                return callback(null);
+            })
+            .catch(function (err) {
                 return callback(err);
-            }
-        );
+            })
     },
 
     getDB: function () {
