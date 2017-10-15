2c31e008d5f7f68bd23f1ead2e6a119de105ea2b
Jun Park <jun.parkc@gmail.com>
2017-10-15 14:13:18 +0900

    Apply ESlint

--- app/db.js
+++ app/db.js
@@ -8,20 +8,17 @@ const MongoClient = require('mongodb');
 let _db;
 
 module.exports = {
-    "connect": function (callback) {
-        MongoClient.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PW + '@ds123400.mlab.com:23400/' + process.env.DB_HOST)
-            .then(function (db) {
+    connect: function (callback) {
+        MongoClient.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PW}@ds123400.mlab.com:23400/${process.env.DB_HOST}`)
+            .then((db) => {
                 _db = db;
-                logger.debug('DB connection successful.')
+                logger.debug('DB connection successful.');
                 return callback(null);
             })
-            .catch(function (err) {
-                logger.debug('DB connection failure.')
+            .catch((err) => {
+                logger.error('DB connection failure.');
                 return callback(err);
-            })
+            });
     },
-
-    "getDB": function () {
-        return _db;
-    }
+    getDB: function () { return _db; },
 }
