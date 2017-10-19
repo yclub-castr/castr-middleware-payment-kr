08aa654a3d3c30c9dc68f718cdae9e4b9030a694
Jun Park <jun.parkc@gmail.com>
2017-10-19 14:27:32 +0900

    Update encryption strategy

--- app/menucast/mc.service.js
+++ app/menucast/mc.service.js
@@ -22,25 +22,37 @@ class MenucastService {
         // Find promotable from 'promotions.promoTable'
         const business_id = req.params.business_id;
         const promotable_id = req.params.promotable_id;
-        const name = {
-            short: null,
-            long: `Menucast coupon redeem [#${business_id}] - ${promotable_id}`,
-            long_kr: `메뉴캐스트 쿠폰 사용 [#${business_id}] - ${promotable_id}`,
-        };
-
-        mongoDB.getDB().collection('mc-transactions').insertOne({
-            business_id: business_id,
-            mc_customer_id: req.body.mc_customer_id,
-            type: payment_type.mc_redeem,
-            name: name,
-            promotable_name: req.body.promotable_name,
-            currency: 'KRW',
-            amount: req.body.amount,
-            perc_disc_applied: req.body.discount,
-            status: status_type.redeemed,
-            time_redeemed: new Date(),
-            time_created: new Date(),
-        })
+        mongoDB.getDB().collection('promotions').findOne({ promoTable: { $elemMatch: { _id: mongoDB.ObjectId(promotable_id) } } })
+            .then((promotion) => {
+                let promotable_name;
+                let amount;
+                let perc_disc_applied;
+                promotion.promoTable.forEach((promotable) => {
+                    if (promotable._id.toString() === promotable_id) {
+                        promotable_name = promotable.nameOne;
+                        amount = (promotable.price * (1 - (promotable.discount / 100))).toFixed(0);
+                        perc_disc_applied = promotable.discount;
+                    }
+                });
+                const name = {
+                    short: null,
+                    long: `Menucast coupon redeem [#${business_id}] - ${promotable_name}`,
+                    long_kr: `메뉴캐스트 쿠폰 사용 [#${business_id}] - ${promotable_name}`,
+                };
+                return mongoDB.getDB().collection('mc-transactions').insertOne({
+                    business_id: business_id,
+                    promotable_id: promotable_id,
+                    promotable_name: promotable_name,
+                    mc_customer_id: req.body.mc_customer_id,
+                    type: payment_type.mc_redeem,
+                    name: name,
+                    currency: 'KRW',
+                    amount: amount,
+                    perc_disc_applied: perc_disc_applied,
+                    status: status_type.redeemed,
+                    time_created: new Date(),
+                });
+            })
             .then(() => {
                 const msg = `Coupon redeem for promotable (#${promotable_id}) successfully saved to DB`;
                 logger.debug(msg);
